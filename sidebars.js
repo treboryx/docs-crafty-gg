e80ab3f2257fb96032c97c5e0e19066ca6c073cd
Robert <treboryx@gmail.com>
2021-03-13 16:28:29 +0200

    Initial commit 🚀

---
+++ sidebars.js
@@ -0,0 +1,8 @@
+module.exports = {
+  someSidebar: {
+    "Crafty Discord Bot": ["doc1", "doc2", "doc3"],
+    "Crafty API": ["doc1", "doc2", "doc3"],
+    "mc-stats": ["doc1", "doc2", "doc3"],
+    Features: ["mdx"],
+  },
+};
